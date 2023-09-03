ed11f3cb25d4df58faa529bd5d2fb112b5133098
boi <119715263+cpp-johnny@users.noreply.github.com>
2023-09-03 12:00:29 +0800

    Update selection-of-data-frame-elements.r

--- selection-of-data-frame-elements.r
+++ selection-of-data-frame-elements.r
@@ -5,3 +5,15 @@ planets_df[1, 3]
 
 # Print out data for Mars (entire fourth row)
 planets_df[4,] # note: end with [4,], not [4]
+
+
+
+# for selction of data in another type,put  
+planets_df[1:3,"type"]
+
+# example:
+
+# The planets_df data frame from the previous exercise is pre-loaded
+
+# Select first 5 values of diameter column
+planets_df[1:5, "diameter"]
